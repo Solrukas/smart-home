910574b4cc2ce27ce559c9e5e656bbd6e357d1a8
hhshin <gusgh1824@naver.com>
2023-05-30 20:39:50 +0900

    Add 'start' function with Telegram bot commands

--- server/chat/bots.js
+++ server/chat/bots.js
@@ -11,3 +11,19 @@ function start() {
         const chatId = msg.chat.id;
         bot.sendMessage(chatId, '===명령어 사용법===\n/help : commands\n/items : item list\n/run <item_id> : item on/off');
     });
+
+	bot.onText(/\/items/, (msg) => {
+        const chatId = msg.chat.id;
+        bot.sendMessage(chatId, '===Item List===\n' + item_id.map((item, index) => `${index + 1}. id:${item.id} / name:${item.name}`).join('\n'));
+    });
+
+    bot.onText(/\/run (.+)/, (msg, match) => {
+        const chatId = msg.chat.id;
+        bot.sendMessage(chatId, "");
+    });
+
+}
+
+module.exports = {
+    start: start,
+};
