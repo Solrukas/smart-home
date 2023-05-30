a73647ae123550badb7774532779d156083c201d
hhshin <gusgh1824@naver.com>
2023-05-30 20:21:17 +0900

    Add Telegram bot initialization and item ID retrieval

--- server/chat/bots.js
+++ server/chat/bots.js
@@ -1,2 +1,7 @@
 const { token, chatld } = require('./telegram.json');
-const TelegramBot = requite('node-telegram-bot-api');
+const TelegramBot = require('node-telegram-bot-api');
+
+const check = require('./check');
+const item_id = check.checkId;
+
+const bot = new TelegramBot(token, { polling: true });
